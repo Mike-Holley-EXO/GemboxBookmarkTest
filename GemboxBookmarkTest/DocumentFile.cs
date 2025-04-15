a69809b5fb8c61164de6ecd008e0aa16603f31ab
Mike-Holley-EXO <mike@exodesk.com>
2025-04-15 16:32:48 +1200

    Add project files.

---
+++ GemboxBookmarkTest/DocumentFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemboxBookmarkTest
+{
+    public partial class DocumentFile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string FileName { get; set; }
+
+        [Required]
+        public byte[] FileData { get; set; }
+
+        [Timestamp]  // Indicates that this property should be treated as a concurrency token
+        public byte[] RowVersion { get; set; }
+    }
+}
---
+++ GemboxBookmarkTest/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using GemBox.Document;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GemboxBookmarkTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            GemBox.Document.ComponentInfo.SetLicense(BundleKeyTextBox.Text);
+            string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DocumentTemplate", "Template.docx");
+
+            // Use this for Windows Authentication with certificate trust
+            DocumentModel document = DocumentModel.Load(filePath);
+            var bookmarks = document.Bookmarks.ToList();
+            var document2 = document.Clone(true);
+            var bookmarks2 = document2.Bookmarks.ToList();
+        }
+    }
+}
